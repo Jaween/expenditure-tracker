a07530cafc3491931f3d473582b0a928b26ceb2f
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2018-11-15 13:58:58 +1030

    Update create screen

--- lib/create_bloc.dart
+++ lib/create_bloc.dart
@@ -1,4 +1,7 @@
-import 'package:expenditure_tracker/purchase.dart';
+import 'dart:async';
+
+import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
 
 class CreateBloc {
 
@@ -12,6 +15,50 @@ class CreateBloc {
   int amount = 0;
   String currency = "AUD";
 
-  CreateBloc();
+  final _dateSink = StreamController<DateTime>();
+  Sink<DateTime> get dateSink => _dateSink.sink;
 
+  static final _dateFormat = DateFormat.yMMMd();
+  final _formattedDateStream = BehaviorSubject<String>(seedValue: _dateFormat.format(DateTime.now()));
+  Stream<String> get formattedDateStream => _formattedDateStream;
+
+  final _amountSink = StreamController<String>();
+  Sink<String> get amountSink => _amountSink.sink;
+
+  final _amountStream = BehaviorSubject<Data<int>>();
+  Stream<Data<int>> get amountStream => _amountStream;
+
+  CreateBloc() {
+    _dateSink.stream.listen((date) {
+      this.date = date;
+      _formattedDateStream.add(_dateFormat.format(date));
+    });
+
+    _amountSink.stream.listen((amount) {
+      int amountInt = int.tryParse(amount);
+      if (amountInt == null) {
+        _amountStream.add(Data(Status.Error, amountInt, "Enter an amount greater than or equal to 0"));
+      } else {
+        _amountStream.add(Data(Status.Ok, amountInt));
+      }
+    });
+  }
+
+  void dispose() {
+    _dateSink.close();
+    _amountSink.close();
+  }
+}
+
+enum Status {
+  Ok, Error
+}
+
+class Data<T> {
+
+  final Status status;
+  final T data;
+  final String errorMessage;
+
+  Data(this.status, this.data, [this.errorMessage]);
 }
--- lib/create_screen.dart
+++ lib/create_screen.dart
@@ -4,10 +4,13 @@ import 'package:expenditure_tracker/interface/repository.dart';
 import 'package:flutter/material.dart';
 
 class CreateScreen extends StatefulWidget {
+  final Repository repository;
 
-  final Repository _repository;
-
-  CreateScreen(this._repository);
+  CreateScreen(
+    this.repository, {
+    Key key,
+  })  : assert(repository != null),
+        super(key: key);
 
   @override
   CreateScreenState createState() {
@@ -16,7 +19,6 @@ class CreateScreen extends StatefulWidget {
 }
 
 class CreateScreenState extends State<CreateScreen> {
-
   CreateBloc _createBloc;
 
   @override
@@ -27,7 +29,7 @@ class CreateScreenState extends State<CreateScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("Create"),
         actions: <Widget>[
@@ -40,157 +42,218 @@ class CreateScreenState extends State<CreateScreen> {
           )
         ],
       ),
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _createWithIcon(Icons.category, _createCategoryChips(context)),
+            _createWithIcon(Icons.description, _createDescription(context)),
+            _createWithIcon(Icons.date_range, _createDate(context)),
+            _createWithIcon(Icons.place, _createWhere(context)),
+            _createWithIcon(Icons.monetization_on, _createAmount(context)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _createWithIcon(IconData data, Widget child) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding:
+              const EdgeInsets.only(left: 16.0, right: 32, top: 32, bottom: 8),
+          child: Icon(
+            data,
+            color: Colors.black.withOpacity(0.38),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: child,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _createCategoryChips(BuildContext context) {
+    return Container(
+      height: 56.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-            child: Container(
-              height: 40.0,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  _createCategoryChip("Food"),
-                  _createCategoryChip("Transport"),
-                  _createCategoryChip("Drinks"),
-                  _createCategoryChip("Accommodation"),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              onChanged: (value) => setState(() { _createBloc.description = value; }),
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Description"
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    child: Text(_createBloc.date.toString())
-                ),
-                RaisedButton(
-                  onPressed: () async {
-                    final date = await showDatePicker(
-                      context: context,
-                      initialDate: _createBloc.date,
-                      firstDate: DateTime(2010),
-                      lastDate: DateTime.now());
-                    setState(() {
-                      _createBloc.date = date;
-                    });
-                  },
-                  child: Text("When"),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              onChanged: (value) => setState(() { _createBloc.locationName = value; }),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Where"
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    onChanged: (value) => setState(() { _createBloc.amount = num.parse(value).toInt(); }),
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Amount"
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: DropdownButton<String>(
-                    value: _createBloc.currency,
-                    onChanged: (value) => setState(() { _createBloc.currency = value; }),
-                    items: <String>["AUD", "USD", "LKR"].map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value)
-                      );
-                    }).toList()
-                  ),
-                ),
-              ],
-            ),
-          ),
+          _createCategoryChip("Food"),
+          _createCategoryChip("Transport"),
+          _createCategoryChip("Drinks"),
+          _createCategoryChip("Accommodation"),
         ],
       ),
     );
   }
 
   Widget _createCategoryChip(String text) {
-    return ChoiceChip(
-      label: Text(text),
-      selected: _createBloc.category == text,
-      avatar: CircleAvatar(
-        child: FlutterLogo(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: ChoiceChip(
+        label: Text(text),
+        selected: _createBloc.category == text,
+        avatar: CircleAvatar(child: Icon(Icons.directions_transit)),
+        onSelected: (selected) {
+          if (selected) {
+            setState(() {
+              _createBloc.category = text;
+            });
+          }
+        },
       ),
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      onSelected: (selected) {
-        if (selected) {
-          setState(() {
-            _createBloc.category = text;
-          });
-        }
-      },
+    );
+  }
+
+  Widget _createDescription(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: TextField(
+        onChanged: (value) => setState(() {
+              _createBloc.description = value;
+            }),
+        decoration: InputDecoration(
+            border: OutlineInputBorder(), hintText: "Description"),
+      ),
+    );
+  }
+
+  Widget _createDate(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: InkWell(
+        onTap: () async {
+          final date = await showDatePicker(
+            context: context,
+            initialDate: _createBloc.date,
+            firstDate: DateTime(2010),
+            lastDate: DateTime.now().add(Duration(days: 365 * 10)));
+          if (date != null) {
+            setState(() {
+              _createBloc.date = date;
+            });
+          }
+        },
+        child: Container(
+          height: 56.0,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: StreamBuilder<String>(
+                  stream: _createBloc.formattedDateStream,
+                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Text("No data");
+                    }
+                    return Text(snapshot.data);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _createWhere(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: TextField(
+        onChanged: (value) => setState(() {
+              _createBloc.locationName = value;
+            }),
+        decoration:
+            InputDecoration(border: OutlineInputBorder(), hintText: "Where"),
+      ),
+    );
+  }
+
+  Widget _createAmount(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<Data<int>>(
+                stream: _createBloc.amountStream,
+                builder:
+                    (BuildContext context, AsyncSnapshot<Data<int>> snapshot) {
+                  return TextField(
+                    onChanged: (value) => setState(() {
+                          _createBloc.amountSink.add(value);
+                        }),
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        errorText: snapshot.data?.errorMessage,
+                        hintText: "Amount"),
+                  );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: DropdownButton<String>(
+                value: _createBloc.currency,
+                onChanged: (value) => setState(() {
+                      _createBloc.currency = value;
+                    }),
+                items: <String>["AUD", "USD", "LKR"].map((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
+                }).toList()),
+          ),
+        ],
+      ),
     );
   }
 
   void save(BuildContext outerContext) async {
     final purchase = Purchase(
-      _createBloc.category,
-      _createBloc.description,
-      _createBloc.date,
-      _createBloc.latitude,
-      _createBloc.longitude,
-      _createBloc.locationType,
-      _createBloc.locationName,
-      _createBloc.amount,
-      _createBloc.currency
-    );
+        _createBloc.category,
+        _createBloc.description,
+        _createBloc.date,
+        _createBloc.latitude,
+        _createBloc.longitude,
+        _createBloc.locationType,
+        _createBloc.locationName,
+        _createBloc.amount,
+        _createBloc.currency);
 
-    var wait = widget._repository.createOrUpdatePurchase(purchase);
-    showDialog(context: outerContext, builder: (BuildContext context) {
-      wait.then((_) {
-        Navigator.pop(context);
-        Navigator.of(outerContext).pop();
-      });
-      return Dialog(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: CircularProgressIndicator()
-              ),
+    var wait = widget.repository.createOrUpdatePurchase(purchase);
+    showDialog(
+        context: outerContext,
+        builder: (BuildContext context) {
+          wait.then((_) {
+            Navigator.pop(context);
+            Navigator.of(outerContext).pop();
+          });
+          return Dialog(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: CircularProgressIndicator()),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
-    });
+          );
+        });
   }
 }
