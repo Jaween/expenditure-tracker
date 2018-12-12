602e6d8c5005bfb872f311cdafac8ed9330ba1bf
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2018-12-12 12:28:12 +1030

    Add photo and linked providers to User

--- lib/interface/concrete/firebase_type_user.dart
+++ lib/interface/concrete/firebase_type_user.dart
@@ -2,7 +2,6 @@ import 'package:expenditure_tracker/interface/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseTypeUser extends User {
-
   final FirebaseUser _firebaseUser;
 
   FirebaseTypeUser(this._firebaseUser);
@@ -15,4 +14,14 @@ class FirebaseTypeUser extends User {
 
   @override
   bool get isAnonymous => _firebaseUser.isAnonymous;
+
+  @override
+  String get photoUrl => _firebaseUser.photoUrl;
+
+  @override
+  List<String> get linkedProviderIds =>
+      _firebaseUser.providerData
+        .map((userInfo) => userInfo.providerId)
+        .where((provider) => provider != "firebase")
+        .toList();
 }
--- lib/interface/user.dart
+++ lib/interface/user.dart
@@ -2,4 +2,6 @@ abstract class User {
   String get userId;
   String get displayName;
   bool get isAnonymous;
+  String get photoUrl;
+  List<String> get linkedProviderIds;
 }
